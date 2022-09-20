617beb275f0a3304aa34ed1b50efd34c846f96dc
snoles27 <108779940+snoles27@users.noreply.github.com>
2022-09-20 15:45:59 -0400

    propellant as seperate mass element

--- massElement.cpp
+++ massElement.cpp
@@ -5,17 +5,20 @@ class massElement{
     private:
         Eigen::Vector3d rGR_B;
         double mass;
+        Eigen::Matrix3d Ig;
 
     public:
         //constructor
-        massElement(Eigen::Vector3d pos, double m){
+        massElement(Eigen::Vector3d pos, double m, Eigen::Matrix3d I){
             rGR_B = pos;
             mass = m;
+            Ig = I;
         }
 
         massElement(){
             rGR_B = Eigen::Vector3d (0.0,0.0,0.0);
             mass = 0;
+            Ig = Eigen::Matrix3d ();
         }
 
         //gets
@@ -28,6 +31,7 @@ class massElement{
         Eigen::Vector3d getLocation(){
             return rGR_B;
         }
+        
 
     
 };
